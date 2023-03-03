603845575a0f1c435abbb3935797b9af03761cab
Ahmed Maher <zamalek.545r5@gmail.com>
2023-03-04 01:09:30 +0200

    splash view done

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-abstract class AppColors
-{
-  static const kScaffoldBgColor =Color(0xffF9F9F9);
+abstract class AppColors {
+  static const kScaffoldBgColor = Color(0xffF9F9F9);
+  static const kElevatedButtonColor = Color(0xffDB3022);
 }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,14 +1,26 @@
+import 'package:e_commerce_app/views/pages/home_view.dart';
 import 'package:e_commerce_app/views/pages/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRoutes {
+
+  static const kHomeViewRoute='/homeView';
+
+
+
  static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return const SplashView();
+        },
+      ),
+       GoRoute(
+        path: kHomeViewRoute,
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeView();
         },
       ),
     ],
