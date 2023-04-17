ea6639566051c9e53360af238030c365576e3d7e
Ahmed Maher <zamalek.545r5@gmail.com>
2023-04-17 03:32:20 +0200

    refactor splash view

--- lib/features/splash/widgets/splash_view_body.dart
+++ lib/features/splash/widgets/splash_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../core/utils/app_routes.dart';
 import '../../../core/utils/asset_data.dart';
+import '../../../core/utils/service_locator.dart';
 import '../../auth/repos/auth_repo_imp.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -30,24 +30,14 @@ class _SplashViewBodyState extends State<SplashViewBody> {
   }
 
   void navToHome() {
-    final repo = GetIt.I.get<AuthRepoImp>();
-    repo.authStateChanges().listen((event) {
-      event != null
-          ? Future.delayed(const Duration(seconds: 5), () {
-              if (mounted) {
-                GoRouter.of(context)
-                    .pushReplacement(AppRoutes.kNavBarViewRoute);
-              }
-            })
-          : Future.delayed(
-              const Duration(seconds: 5),
-              () {
-                if (mounted) {
-                  GoRouter.of(context)
-                      .pushReplacement(AppRoutes.kAuthViewRoute);
-                }
-              },
-            );
+     final repo = getIt.get<AuthRepoImp>();
+
+    Future.delayed(const Duration(seconds: 5), () {
+      repo.authStateChanges().listen((user) {
+        user != null
+            ? GoRouter.of(context).pushReplacement(AppRoutes.kNavBarViewRoute)
+            : GoRouter.of(context).pushReplacement(AppRoutes.kAuthViewRoute);
+      });
     });
   }
 }
