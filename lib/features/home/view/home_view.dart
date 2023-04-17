52268f7c65d146e6028eddf3fb72973d536d0f7b
Ahmed Maher <zamalek.545r5@gmail.com>
2023-04-17 08:58:46 +0200

    replace stream builder with cubit in home view

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/home/manager/new_products_cubit/new_products_cubit.dart';
 import 'package:e_commerce_app/features/home/manager/sale_products_cubit/sale_products_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,8 +10,15 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<SaleProductsCubit>(
-      create: (context) => SaleProductsCubit()..getSaleProducts(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SaleProductsCubit>(
+          create: (context) => SaleProductsCubit()..getSaleProducts(),
+        ),
+        BlocProvider<NewProductsCubit>(
+          create: (context) => NewProductsCubit()..getNewProduct(),
+        ),
+      ],
       child: const HomeViewBody(),
     );
   }
--- lib/features/home/view/widgets/new_products_list_view.dart
+++ lib/features/home/view/widgets/new_products_list_view.dart
@@ -1,12 +1,11 @@
+import 'package:e_commerce_app/features/home/manager/new_products_cubit/new_products_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/global/themes/app_colors/app_colors_light.dart';
-import '../../../../core/utils/service_locator.dart';
 import '../../../../core/widgets/custom_progress_indicator.dart';
 import '../../../../core/widgets/products_list_view_item.dart';
-import '../../model/product_model.dart';
-import '../../repo/home_repo_imp.dart';
 
 class NewProductsListView extends StatelessWidget {
   const NewProductsListView({super.key});
@@ -16,35 +15,35 @@ class NewProductsListView extends StatelessWidget {
     return SizedBox(
       height: 285.h,
       width: double.infinity,
-      child: StreamBuilder<List<ProductModel>>(
-          stream: getIt.get<HomeRepoImp>().getNewProducts(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              final products = snapshot.data;
-              if (products == null || products.isEmpty) {
-                return Center(
-                  child: Text(
-                    'No data available',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge
-                        !.copyWith(color: AppColorsLight.kPrimaryColor),
-                  ),
-                );
-              }
-              return ListView.separated(
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) =>
-                    ProductsListViewItem(snapshot.data![index]),
-                itemCount: products.length,
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (context, index) => SizedBox(
-                  width: 18.w,
-                ),
-              );
-            }
-            return const CustomProgressIndicator();
-          }),
+      child: BlocBuilder<NewProductsCubit, NewProductsState>(
+        builder: (context, state) {
+          if (state is NewProductsFailure) {
+            return Center(
+              child: Text(
+                'No data available',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: AppColorsLight.kPrimaryColor,
+                    ),
+              ),
+            );
+          }
+          if (state is NewProductsSuccess) {
+            return ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) => ProductsListViewItem(
+                state.newProductsList.elementAt(index),
+              ),
+              itemCount: state.newProductsList.length,
+              scrollDirection: Axis.horizontal,
+              separatorBuilder: (context, index) => SizedBox(
+                width: 18.w,
+              ),
+            );
+          }
+
+          return const CustomProgressIndicator();
+        },
+      ),
     );
   }
 }
